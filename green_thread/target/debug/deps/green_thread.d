0a1f5b5ca9e5dc3cf315fe350fc31f6aa4228ffc
zhangly1234 <76698406+zhangly1234@users.noreply.github.com>
2021-09-20 14:18:40 +0800

    Add files via upload

---
+++ green_thread/target/debug/deps/green_thread.d
@@ -0,0 +1,5 @@
+D:\rust_project\green_thread\target\debug\deps\green_thread.exe: src\main.rs
+
+D:\rust_project\green_thread\target\debug\deps\green_thread.d: src\main.rs
+
+src\main.rs:
---
+++ green_thread/target/debug/green_thread.d
@@ -0,0 +1,1 @@
+D:\rust_project\green_thread\target\debug\green_thread.exe: D:\rust_project\green_thread\src\main.rs
